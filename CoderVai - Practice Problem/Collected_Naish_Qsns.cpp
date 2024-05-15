4f411034951e13c1edced742a9bcf87441a6a11b
Mi Taseen <114951279+InferiorAK@users.noreply.github.com>
2024-05-15 14:57:43 +0600

    Update Collected_Naish_Qsns.cpp

--- CoderVai - Practice Problem/Collected_Naish_Qsns.cpp
+++ CoderVai - Practice Problem/Collected_Naish_Qsns.cpp
@@ -73,3 +73,73 @@ using namespace std;
 // // It Will give output: 10
 
 // *-------------------------------------------------------*-------------------------------------------------------*
+
+//         // Question-03 : What is the output of this code?
+
+// #include <iostream>
+// int main() {
+//     int arr[5] = {11, 22, 33, 44, 55};
+//     int* ptr = arr;
+//     std::cout << *ptr << std::endl;
+//     return 0;
+// }
+// a) 11
+// b) 55
+// c) Error
+// d) Garbage value
+
+// Ans: 11
+// It will give the 1st value of the array. Because, ptr is pointing to the 1st element of the array.
+
+// *-------------------------------------------------------*-------------------------------------------------------*
+
+//         // Question-04 : What is the range of index values of the following array?
+
+//             int arr[1000];
+//             (a) 1 to 1000
+//             (b) 1 to 999
+//             (c) 0 to 999
+//             (d) 0 to 1000
+//          Answer:
+//         (c) 0 to 999
+
+// Explanation: 
+
+// If the size of an array is n , then the valid index range of the array is 0 to n-1 .
+
+
+// *-------------------------------------------------------*-------------------------------------------------------*
+
+
+//          // Question-05 : What is the output of this code?
+
+            // #include <iostream>
+
+            // void modifyArray(int arr[]) {
+            //     arr[1] = 10;
+            // }
+
+            // int main() {
+            //     int arr[3] = {1, 2, 3};
+            //     modifyArray(arr);
+            //     std::cout << arr[1] << std::endl;
+            //     return 0;
+            // }
+
+            // a) 1
+            // b) 10
+            // c) 2
+            // d) Error
+
+            // Answer:
+            // b) 10
+
+// Explanation: 
+// The function modifyArray modifies the array arr passed to it, changing arr[1] to 10.
+
+// In C++, when you pass an array to a function, you are actually passing a pointer to the first element of the array. This means you are passing a reference to the array's data in memory, not a copy of the entire array.
+
+// So, when you modify the elements of the array inside the function, those changes are reflected in the original array in the calling function. This behavior is often referred to as "passing by reference" for arrays because you are working directly with the original data in memory.
+
+// *-------------------------------------------------------*-------------------------------------------------------*    
+
