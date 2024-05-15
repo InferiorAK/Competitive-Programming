3caaf6b783658faa48c7eeaa1d6d304c54b921f3
Mi Taseen <114951279+InferiorAK@users.noreply.github.com>
2024-05-15 14:58:07 +0600

    Update test.cpp

--- CoderVai - Practice Problem/test.cpp
+++ CoderVai - Practice Problem/test.cpp
@@ -1,39 +1,140 @@
 // #include <iostream>
 // #include <cmath>
 #include <bits/stdc++.h>
+#include <pyfeel.h>
 using namespace std;
+
+// int increase(int &x)
+// {
+//     x += 1;
+//     cout << &x << endl;
+// }
+
+// int main()
+// {
+//     // for (int j = 0; j <= 5; j++)
+//     // {
+//     //     cout << j << endl;
+//     // }
+
+//     // cout << round(2.6);
+//     // return 0;
+
+//     // bool isCodingFun = true;
+//     // bool isFishTasty = false;
+//     // cout << isCodingFun << endl;
+//     // cout << isFishTasty;
+
+//     // cout << sinh(1);
+
+//     // for (int i = 0; i < 10; i++)
+//     // {
+//     //     if (i == 5)
+//     //     {
+//     //         continue;
+//     //     }
+//     //     cout << i << endl;
+//     // }
+//     // cout << "done";
+
+//     // int arr[5];
+//     // for (int i = 0; i < 5; i++)
+//     // {
+//     //     cin >> arr[i];
+//     // }
+//     // cout << sizeof(arr);
+
+//     // int x = 5, y = 10;
+//     // int a;
+//     // a = y;
+//     // y = x;
+//     // x = a;
+//     // increase(x);
+//     // cout << x << " " << y;
+
+//     // char arr[] = {'A', 'B', 'C', '\0', 'E', 'F'};
+//     // cout << arr[4];
+
+//     // char arr[7] = "String";
+//     // char arr2[8] = "String2";
+
+//     // // cout << sizeof(arr);
+//     // // cout << strcmp(arr, arr2);
+//     // // cout << arr;
+//     // // strcpy (arr, arr2);
+
+//     // cout << strlwr(arr) << endl;
+//     // cout << strupr(arr);
+// }
+
+// int arr_print(int arr[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// int main()
+// {
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     arr_print(arr, sizeof(arr) / sizeof(arr[0]));
+// }
 
 int main()
 {
-    // for (int j = 0; j <= 5; j++)
+    // print("hfnrhfa");
+    // string s = "hdgyewg";
+    // cout << s[5];
+
+    // string test = "Test";
+    // transform(test.begin(), test.end(), test.begin(), ::toupper);
+    // sort(test.begin(), test.end());
+    // cout << test;
+
+    // int i = 0;
+    // for (int row = 0; row < 5; row++)
     // {
-    //     cout << j << endl;
+    //     cout << i;
+    //     i++;
     // }
 
-    // cout << round(2.6);
-    // return 0;
+    // cout << abs(-5);
+    // int s[5] = {1, 2, 3, 4, 5};
+    // cout << s[4];
 
-    // bool isCodingFun = true;
-    // bool isFishTasty = false;
-    // cout << isCodingFun << endl;
-    // cout << isFishTasty;
+    // cout << 3 % 2;
+    // int arr[10] = {1, 2, 3, 4};
+    // cout << sizeof(arr)/ sizeof(arr[0]);
 
-    // cout << sinh(1);
-
-    // for (int i = 0; i < 10; i++)
+    // int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    // cout << "The array is: ";
+    // for (auto i : arr)
     // {
-    //     if (i == 5)
+    //     cout << i << " ";
+    // }
+
+    // int arr[10] = {1, 2, 4, 5, 9, 8, 7, 2, 3, 3};
+    // for (int x = 0; x <= 10; x++)
+    // {
+    //     for (auto item : arr)
     //     {
-    //         continue;
+    //         if (x != item)
+    //         {
+    //             cout << x;
+    //         }
     //     }
-    //     cout << i << endl;
     // }
-    // cout << "done";
 
-    int arr[5];
-    for (int i = 0; i < 5; i++)
-    {
-        cin >> arr[i];
-    }
-    cout << sizeof(arr);
+    // int arr[10] = {1, 2, 4, 5, 9, 8, 7, 2, 3, 3};
+    // int arr2[10] = {4, 5, 9, 8, 7, 3, 3};
+
+    // cout << arr - arr2;
+    // int arr[5];
+    // cout << arr[0];
+
+    int arr[5] = {11, 22, 33, 44, 55};
+    int* ptr = arr;
+    std::cout << *ptr << std::endl;
+    return 0;
 }
